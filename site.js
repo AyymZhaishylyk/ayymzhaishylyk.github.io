b3e4a1a8790d06f2d97463bfe05634f780731ec7
AyymZhaishylyk <149085201+AyymZhaishylyk@users.noreply.github.com>
2025-04-06 12:00:13 +0200

    Update site.js

--- site.js
+++ site.js
@@ -1,51 +1,19 @@
-// CV Buttons
-function callBio() {
-  var x = document.getElementById("bio");
+// About Buttons
+function callAbout() {
+  var x = document.getElementById("about");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
-  var x = document.getElementById("bioPush");
+  var x = document.getElementById("aboutPush");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
-  var z = document.getElementById("bioPushCV");
-  if (z.style.display === "none" && x.style.display === "block") 
-  {
-    z.style.display = "block";
-  } 
-  else 
-  {
-    z.style.display = "none";
-  }
-}
-function callCV() {
-  var y = document.getElementById("cv");
-  if (y.style.display === "none") {
-    y.style.display = "block";
-  } else {
-    y.style.display = "none";
-  }
-  var x = document.getElementById("cvPush");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-  var z = document.getElementById("bioPushCV2");
-  var bio = document.getElementById("bio");
-  if (z.style.display === "none" && bio.style.display === "block") 
-  {
-    z.style.display = "block";
-  } 
-  else 
-  {
-    z.style.display = "none";
-  }
-}
+
+
 //Randomizer
 var images = $('.content');
 console.log(images.length);
